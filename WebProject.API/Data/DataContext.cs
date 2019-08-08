e9c7ed228611486a9169fc6927b6d0c130d5d943
ortofone24 <ortofone24@gmail.com>
2019-08-08 21:20:30 +0200

    testCommit

--- WebProject.API/Data/DataContext.cs
+++ WebProject.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace WebProject.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
